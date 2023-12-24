eb244b1bf6712b7cee97fb5ce4c84d10bcdb9b73
nidhidadiya18 <nidhi.dadiya@webrexstudio.com>
2023-12-24 13:29:19 +0530

    Create new tests folder to keep test files

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import App from "./App";
+import App from "../App";
 
 test("test heading", () => {
   render(<App />);
