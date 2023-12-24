ec067f7f4adb19fb58c466244fc03b1b3e614910
nidhidadiya18 <nidhi.dadiya@webrexstudio.com>
2023-12-24 15:44:27 +0530

    Throw err from function

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -5,6 +5,12 @@ export const StringCalculator = (inputString) => {
   // Extract numbers from the string
   var numbersArray = inputString.match(regex);
 
+  // Find negatives
+  const negativeNumbers = numbersArray?.filter((num) => num < 0);
+  if (negativeNumbers?.length > 0) {
+    throw new Error(`negatives not allowed :${negativeNumbers.toString()}`);
+  }
+
   // Sum of matched  strings numbers
   const sum = numbersArray?.reduce((sum, item) => sum + Number(item), 0);
   return sum || 0;
