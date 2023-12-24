898754f33787649dd70dda3a19a308792efc134e
nidhidadiya18 <nidhi.dadiya@webrexstudio.com>
2023-12-24 13:12:28 +0530

    Add basic test of heading text

--- src/App.test.js
+++ src/App.test.js
@@ -1,1 +1,11 @@
 // App.test.js
+
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+test("test heading", () => {
+  render(<App />);
+  expect(screen.getByRole("heading")).toHaveTextContent(/TDD is cool/i);
+});
