75b40ae0ef93b3124aab33593bc8c5c9bec8fc56
nidhidadiya18 <nidhi.dadiya@webrexstudio.com>
2023-12-24 15:43:05 +0530

    Add test case for new lines in string

--- src/Tests/StringCalculator.test.js
+++ src/Tests/StringCalculator.test.js
@@ -17,4 +17,10 @@ describe("StringCalculator Test cases", () => {
   test("given 1,2,3,4 to return 10", () => {
     expect(StringCalculator("1,2,3,4")).toBe(10);
   });
+  test("given 1\n2,3 to return 6", () => {
+    expect(StringCalculator("1\n2,3")).toBe(6);
+  });
+  test("given 2\n7,3\n8 to return 20", () => {
+    expect(StringCalculator("2\n7,3\n8")).toBe(20);
+  });
 });
